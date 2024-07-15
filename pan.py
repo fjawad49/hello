5b07b7c6a8e9b5440afb5acd273b59b5c5f630eb
Fahim Jawad <mrfj2004@gmail.com>
2024-07-15 19:07:21 -0400

    Initial commit

---
+++ pan.py
@@ -0,0 +1,9 @@
+# Create a dictionary using dictionary comprehension
+people = {
+    "Alice": {"age": 25, "occupation": "Student"},
+    "Bob": {"age": 32, "occupation": "Teacher"},
+    "Charlie": {"age": 27, "occupation": "Freelancer"}
+}
+
+people.update({'Alice': 5})
+print(people["A"])  # Output: Dhaka
